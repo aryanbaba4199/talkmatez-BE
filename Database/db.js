ef3fae4b3552ddef5cfcd958655dc204b389ca42
Aryan Dubey <aryanbaba@gmail.com>
2024-09-27 17:47:40 +0530

    tutor update

--- Database/db.js
+++ Database/db.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-// const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 const connectDb = async() => {
     try{
-        const connection = await mongoose.connect('mongodb://0.0.0.0:27017/talkmatez',)
-        // const connection = await mongoose.connect(MONGODB_URI,)
+        // const connection = await mongoose.connect('mongodb://0.0.0.0:27017/talkmatez')
+        const connection = await mongoose.connect(MONGODB_URI)
         console.log("Connected to Database");
     }catch(e){
         console.error("Error connecting to Database ", e);
