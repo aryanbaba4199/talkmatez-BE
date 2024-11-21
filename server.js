d0e1807767380940418b4159d57e5825c0eff11c
Aryan Dubey <aryanbaba@gmail.com>
2024-11-21 17:38:36 +0530

    update

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 const admin = require("firebase-admin");
-const serviceAccount = require("./models/talkmatez-f8850-firebase-adminsdk-aqirh-d4ba80d895.json");
 const errorHandler = require("./middleware/errorHandler");
 const helpers = require("./routes/helpersRoutes");
 const TutorsRoute = require("./routes/tutorsRoute");
@@ -18,7 +17,19 @@ require("dotenv").config();
 db();
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FBTYPE,
+  project_id: process.env.FBPROJECTID,
+  private_key_id: process.env.FB_PRIVATE_KEY_ID, // Add this if it's not already in .env
+  private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines
+  client_email: process.env.FB_CLIENT_EMAIL,
+  client_id: process.env.FB_CLIENT_ID,
+  auth_uri: process.env.FB_AUTH_URI,
+  token_uri: process.env.FB_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FB_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FB_UNIVERSE_DOMAIN || 'googleapis.com',
+  }),
 });
 
 const app = express();
